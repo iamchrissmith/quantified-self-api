add863637c44b5b81141190463a8a1526349e242
Bao Nguyen <rongxanh88@gmail.com>
2017-08-21 08:46:33 -0600

    Change to allow patch and put requests

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module QuantifiedSelfApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :update, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :put, :options]
       end
     end
 
